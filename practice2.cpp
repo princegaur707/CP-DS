b0f4780ee640d6bce821bcde5481c34179aa0bb6
proxy707 <princegaur1998@gmail.com>
2022-08-12 09:20:52 +0530

    files changed

--- practice2.cpp
+++ practice2.cpp
@@ -5,51 +5,42 @@ int main()
 	int t;
 	cin>>t;
 
-	while(t--)
-	{
+	while(t--){
+		//cout<<t<<endl;
 		int n;
 		cin>>n;
-
 		int freq[10]={0};
 
-		for(int i=0; i<n; i++)
-		{
-			int num;
-			cin>>num;
-			num%=10;
-			freq[num]++;
+		for(int i=0; i<n; i++){
+			int x;
+			cin>>x;
+			x=x%10;
+			freq[x]++;			
 		}
 
-		vector<int>arr;
+		vector<int> v;
 
-		for (int i=0; i<10; i++)
-		{
-			for (int j=0; j<min(3,freq[i]); j++)
-			{
-				arr.push_back(i);
+		for(int i=0; i<10; i++){
+			for(int j=0; j<min(3,freq[i]);j++){
+				v.push_back(i);
 			}
 		}
-
-		int l=arr.size();
+		int l=v.size();
 		int flag=0;
-
-		for(int i=0; i<l-2; i++)
-		{
-			for(int j=i+1; j<l-1; j++)
-			{			
-				for(int k=j+1; k<l; k++)
-				{
-					if((arr[i]+arr[j]+arr[k])%10==3)
-					{
+		for(int i=0; i<l; i++){
+			for (int j=i+1; j<l; j++){
+				for(int k=j+1; k<l; k++){
+					if((v[i]+v[j]+v[k])%10==3){
 						flag=1;
-						break;
 					}
 				}
 			}
 		}
-		if (flag==1)
+		if (flag==1){
 			cout<<"YES"<<endl;
+		}
 		else
 			cout<<"NO"<<endl;
+
 	}
 }
