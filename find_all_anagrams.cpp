3b742ea74888f6ab77f154ecea02e8f798ab97c1
proxy707 <princegaur1998@gmail.com>
2022-08-27 11:16:08 +0530

    files added

--- find_all_anagrams.cpp
+++ find_all_anagrams.cpp
@@ -1,10 +1,73 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    string n = "abc";
-    string m = "cab";
-    sort(n.begin(), n.end());
-    cout << n;
-}
-
+//https://leetcode.com/problems/find-all-anagrams-in-a-string/
+class Solution {
+    bool compare(int *counts, int *countp)
+    {
+        for(int i = 0; i < 26; i++)
+        {
+            if(counts[i] != countp[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+public:
+    vector<int> findAnagrams(string s, string p) {
+        int lens = s.length();
+        int lenp = p.length();
+        vector<int>arr;
+        if(lens < len)p
+        {
+            return arr;
+        }
+        int counts[26] = {0};
+        int countp[26] = {0};
+        for(int i = 0; i < lenp; i++)
+        {
+            countp[p[i] - 'a']++;
+        }
+        
+        for(int i = 0; i < lens; i++)
+        {
+            counts[s[i] - 'a']++;
+            if(i >= lenp)
+            {
+                counts[s[i - lenp] - 'a']--;
+            }
+            if(i >= lenp - 1)
+            {
+                if(compare(counts, countp))
+                {
+                    arr.push_back(i - lenp + 1);
+                }
+            }
+        }
+        return arr;
+    }
+};
+//Do by this approach once have knowledge of syntax of c++  
+//class Solution {
+// public:
+//     vector<int> findAnagrams(string s, string p) {
+//         int len1 = s.length();
+//         int len2 = p.length();
+//         vector<int>arr;
+//         vector<string> arr1;
+//         sort(p.begin(), p.end());
+//         for(int i = 0; i < len1 - len2 -1; i++)
+//         {
+//             for(int j = 0; j < len2; j++)
+//             {
+//                 arr1[i][j]= s[i + j];
+//             }
+//         }    
+//         for(int i = 0; i < len1 -len2; i++)
+//         {
+//             for(int j = 0; j < len2; j++)
+//             {
+//                 cout<<arr1[i][j];
+//             }
+//         }
+//         return arr;
+//     }
+// };
--- practice.cpp
+++ practice.cpp
@@ -1,59 +1,26 @@
-#include<bits/stdc++.h>
-using namespace std;
-int length(int n)
-    {
-        int cnt=0;
-        while(n)
-        {
-            cnt++;
-            n/=10;
-        }
-     	return cnt;
-    }
-    
-int number( int num, int flag)
-    {
-    	int n=0;
-        if(flag==0)
-        {	
-        	while(num)
-        	{
-            	n+=9*(10^(num-1));
-            	num-=1;
-            	cout<<n;
-        	}
-    	}
-        else
-        {
-            while(num)
-        	{
-            	n+=1*(10^(num-1));
-            	num-=1;
-        	}
-        }
-    	return(n);
-    }
+// CPP code to demonstrate append(str)
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Function to demonstrate append()
+void appendDemo(string str1, string str2)
+{
+    // Appends str2 in str1
+    str1.append(str2);
+    cout << "Using append() : ";
+    cout << str1 << endl;
+}
+
+// Driver code
 int main()
 {
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int l;
-        cin>>l;
-        int n;
-        cin>>n;
-        int num=number(l,0);
-        cout<<num;
-        if (l==length(num-n))
-         cout<<num-n<<endl;
-        else
-        {
-        	int num1=number(l+1,1);
-        	cout<<num1-n<<endl;
-        }
-        
-    }
+    string str1("Hello World! ");
+    string str2("GeeksforGeeks");
+
+    cout << "Original String : " << str1 << endl;
+    appendDemo(str1, str2);
+
     return 0;
 }
