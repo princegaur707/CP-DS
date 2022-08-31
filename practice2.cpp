b80328596611f73e059dab318983da3858f43619
proxy707 <princegaur1998@gmail.com>
2022-08-31 20:45:39 +0530

    file updated

--- practice2.cpp
+++ practice2.cpp
@@ -1,68 +1,66 @@
 #include<bits/stdc++.h>
 using namespace std;
-int table[105] = {0};
-
-void CalculateTable(string p) {
-    int i = 1;
-    int j = 0;
+int table[1005] = {0};
+void CalcuateTable(string p)
+{
+    int i = 0;
+    int j = 1;
     int lp = p.length();
-    while (i < lp) {
-
-        while (j > 0 and p[i] != p[j]) {
-            j = table[j - 1];
+    while(j < lp)
+    {
+        while(i > 0 and p[i] != p[j])
+        {
+            i = table[i - 1];
         }
-
-        if (p[i] == p[j]) {
-            table[i] = j + 1;
+        if(p[i] == p[j])
+        {
+            table[j] = i + 1;
             i++;
             j++;
-        } else {
-            i++;
+        }
+        else
+        {
+            j++;
         }
     }
 }
-
-
-void KMP(string s, string p) {
-    CalculateTable(p);
-
-    int i = 0;//This is for iterating over string.
-    int j = 0;//This is for iterating over pattern.
+void KMP(string s, string p)
+{
+    CalcuateTable(p);
+    int i = 0;
+    int j = 0;
     int ls = s.length();
     int lp = p.length();
-
-    while (i < ls) {
-
-        while (j > 0 and s[i] != p[j]) {
+    while(i < ls)
+    {
+        while(j > 0 and s[i] != p[j])
+        {
             j = table[j - 1];
         }
-
-        if (s[i] == p[j]) {
+        if(s[i] == p[j])
+        {
             i++;
             j++;
-        } else {
-            //For the first character only you cannot go prior to this.
-            //for s[i]!=p[j] and j==0 you cannot go to the negative index.
+        }
+        else
+        {
             i++;
         }
-        if (j == lp) {
-            cout << i - j << endl;
-            //Starting point of the pattern in the string.
-            j = table[j - 1];
+        if(j == lp)
+        {
+            cout <<i - j << endl;
         }
     }
-    cout << endl;
 }
-
-
-
-
-int main() {
+int main()
+{
     int n;
-    while (cin >> n) {
-        string s, p;
+    while(cin >> n)
+    {
+        string s;
+        string p;
         cin >> p >> s;
         KMP(s, p);
+        cout << endl;
     }
-
 }
