11d4c06efd8c9558647cf0f995c0f5fa86e979fa
proxy707 <princegaur1998@gmail.com>
2022-10-01 02:03:46 +0530

    file added

--- Robin_Hood.cpp
+++ Robin_Hood.cpp
@@ -1,27 +1,54 @@
-// Time Complexity: O(log N)
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main()
-// {
-// 	int n;
-// 	int k;
-// 	cin >> n >> k;
-// 	int arr[n];
-// 	for(int i = 0; i < n; i++)
-// 	{
-// 		cin >> arr[i];
-// 	}
-// 	while (k > 0)
-// 	{
-// 		int maxi = *max_element(arr, arr + n);
-// 		int index1 = find(arr, arr + n, maxi) - arr;
-// 		int mini = *min_element(arr, arr + n);
-// 		int index2 = find(arr, arr + n, mini) - arr;
-// 		arr[index1] -= 1;
-// 		arr[index2] += 1;
-// 		k--;
-// 	}
-// 	int maxi = *max_element(arr, arr + n);
-// 	int mini = *min_element(arr, arr + n);
-// 	cout << maxi - mini;
-// }
+#include<bits/stdc++.h>
+using namespace std;
+long long n,k,l,r,mid,ans,num; 
+long long a[500005],sum,mx=-1e9,mi=1e9;
+int check1(long long x)
+{
+	long long cnt=0;
+	for(int i=1;i<=n;i++)if(a[i]>x)cnt+=(a[i]-x);
+	return cnt<=k;
+}
+int check2(long long x)
+{
+	long long cnt=0;
+	for(int i=1;i<=n;i++)if(a[i]<x)cnt+=(x-a[i]);
+	return cnt<=k;
+}
+int main()
+{
+	cin>>n>>k;
+	for(int i=1;i<=n;i++)
+	{
+		scanf("%lld",&a[i]);
+		mi=min(mi,a[i]);
+		mx=max(mx,a[i]);
+		sum+=a[i];
+	}
+	l=(sum+n-1)/n;
+	r=mx;
+	while(l<=r)
+	{
+		mid=(l+((r-l)>>1));
+		cout << endl << "mid : " << mid << endl;
+		if(check1(mid))
+		{
+			ans=mid;
+			r=mid-1;
+		}
+		else l=mid+1;
+	}
+	l=mi;
+	r=sum/n;
+	while(l<=r)
+	{
+		mid=(l+((r-l)>>1)); 
+		if(check2(mid))
+		{
+			num=mid;
+			l=mid+1;
+		}
+		else r=mid-1;
+	}
+	cout<<ans-num;
+	return 0;
+}
--- practice.cpp
+++ practice.cpp
@@ -1,62 +1,28 @@
+//Time Complexity: O(Nlog N)
 #include<bits/stdc++.h>
 using namespace std;
-int v[100005];
 int main()
 {
-    int n;  //no. of days
-    int m;  //no. of exams
-    cin >> n >> m;
-    int arr[n + 1];
-    for(int i = 1; i <= n; i++)
-    {
-        cin >> arr[i];
-    }
-    int brr[n + 1];
-    int s = 0;
-    for(int i = 1; i <= m; i++)
-    {
-        cin >> brr[i];
-        s += brr[i];
-    }
-    if(n < s + m)
-    {
-        cout << "-1";
-        return 0;
-    }
-    int left = s + m;
-    int right = n;
-    int ans = -1;
-    while(left <= right)
-    {
-        int mid = (left + right) / 2;
-        int k = 0;
-        for(int i = 1; i <= n; i++)
-        {
-            v[i] = 0;
-        }
-        for(int i = mid; i > 0; i--)
-        {
-            if(arr[i] && !v[arr[i]] && i > brr[arr[i]])
-            {
-                k++;
-                v[arr[i]] = 1;
-            }
-        }
-        if(k == m)
-        {
-            right = mid - 1;
-        }
-        else
-        {
-            left = mid + 1;
-        }
-    }
-    if(right == n)
-    {
-        cout << "-1";
-    }
-    else
-    {
-        cout << right + 1;
-    }
+ int n;
+ int k;
+ cin >> n >> k;
+ int arr[n];
+ for(int i = 0; i < n; i++)
+ {
+     cin >> arr[i];
+ }
+ sort(arr,arr+n);
+ while (k > 0)
+ {
+     int maxi = *max_element(arr, arr + n);
+     int index1 = find(arr, arr + n, maxi) - arr;
+     int mini = *min_element(arr, arr + n);
+     int index2 = find(arr, arr + n, mini) - arr;
+     arr[index1] -= 1;
+     arr[index2] += 1;
+     k--;
+ }
+ int maxi = *max_element(arr, arr + n);
+ int mini = *min_element(arr, arr + n);
+ cout << maxi - mini;
 }
