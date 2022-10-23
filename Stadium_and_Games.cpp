a921341dfbbc7f79750842a7b7d00e73b7fe28da
princegaur707 <princegaur1998@gmail.com>
2022-10-23 23:30:24 +0530

    file added

--- Stadium_and_Games.cpp
+++ Stadium_and_Games.cpp
@@ -1,3 +1,5 @@
+//https://codeforces.com/contest/325/problem/B
+//Other's answer at bottom
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
@@ -37,3 +39,54 @@ int32_t main()
 		cout << "-1";
 	}
 }
+/*#include <iostream>
+#include <string.h>
+#include <algorithm>
+#include <stdio.h>
+ 
+using namespace std;
+typedef long long LL;
+const int N = 1005;
+const LL INF = 1e18;
+const LL R = 3*1e9;
+ 
+LL S[N];
+int cnt;
+ 
+void Binary_Search(LL n)
+{
+    for(int k=0;k<63;k++)
+    {
+        LL l = 1;
+        LL r = R;
+        if(k >= 30) r = INF >> k;
+        while(l <= r)
+        {
+            LL m = (l + r) >> 1;
+            LL ans = (((LL)1 << k) - 1) * m * 2 + m * (m - 1);
+            if(ans > 2*n) r = m - 1;
+            else if(ans < 2*n) l = m + 1;
+            else
+            {
+                if(m&1) S[cnt++] = m * ((LL)1 << k);
+                break;
+            }
+        }
+    }
+}
+ 
+int main()
+{
+    LL n;
+    cin>>n;
+    cnt = 0;
+    Binary_Search(n);
+    sort(S,S+cnt);
+    if(cnt == 0) puts("-1");
+    else
+    {
+        for(int i=0;i<cnt;i++)
+           cout<<S[i]<<endl;
+    }
+    return 0;
+}*/
--- practice.cpp
+++ practice.cpp
@@ -1,32 +1,51 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+#include <stdio.h>
+ 
 using namespace std;
+typedef long long LL;
+const int N = 1005;
+const LL INF = 1e18;
+const LL R = 3*1e9;
+ 
+LL S[N];
+int cnt;
+ 
+void Binary_Search(LL n)
+{
+    for(int k=0;k<63;k++)
+    {
+        LL l = 1;
+        LL r = R;
+        if(k >= 30) r = INF >> k;
+        while(l <= r)
+        {
+            LL m = (l + r) >> 1;
+            LL ans = (((LL)1 << k) - 1) * m * 2 + m * (m - 1);
+            if(ans > 2*n) r = m - 1;
+            else if(ans < 2*n) l = m + 1;
+            else
+            {
+                if(m&1) S[cnt++] = m * ((LL)1 << k);
+                break;
+            }
+        }
+    }
+}
+ 
 int main()
 {
-    int q;
-    cin >> q;
-    while(q--)
+    LL n;
+    cin>>n;
+    cnt = 0;
+    Binary_Search(n);
+    sort(S,S+cnt);
+    if(cnt == 0) puts("-1");
+    else
     {
-        int n;
-        int m;
-        cin >> n >> m;
-        int range1 = m;
-        int range2 = m;
-        int pt = 0;
-        int flag = 1;
-        for(int i = 0; i < n; i++)
-        {
-            int t;
-            int l;
-            int h;
-            cin >> t >> l >> h;
-            range1 = max(l, range1 - (t - pt));
-            range2 = min(h, range2 + (t - pt));
-            if(range1 > range2)
-            {
-                flag = 0;
-            }
-            pt = t;
-        }
-        cout << (flag?"YES\n" : "NO\n");
+        for(int i=0;i<cnt;i++)
+           cout<<S[i]<<endl;
     }
+    return 0;
 }
