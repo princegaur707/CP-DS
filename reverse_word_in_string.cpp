aade48ad4933d131b8d44d818b827ab5eeed9ce7
princegaur707 <princegaur1998@gmail.com>
2022-11-02 23:42:36 +0530

    file updated

--- reverse_word_in_string.cpp
+++ reverse_word_in_string.cpp
@@ -2,16 +2,18 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        char *token = strtok((char*)s.c_str(), " ");
         vector<string>v;
+        char *c = &s[0]; //conversting string to character array
+        char *token = strtok(c, " "); //generating token
         while(token != NULL)
         {
             v.push_back(token);
-            token = strtok(NULL, " ");
+            token = strtok(NULL," ");//we don't need to give char array again it will already have the 
+                                    //previous one stored.
         }
-        reverse(v.begin(), v.end());
-        string ans = "";
+        reverse(v.begin(), v.end());//reversing the words
         int n = v.size();
+        string ans = "";
         for(int i = 0; i < n; i++)
         {
             if(i == n - 1)
@@ -26,3 +28,17 @@ public:
         return ans;
     }
 };
+/*
+In Python we can use split function directly but in c++
+strtok: works only for character array so firstly need to convert the string to character array
+We cannot use vector directly as it will pick up every character one by one
+t
+h
+e
+ 
+s
+k
+y
+
+so we need to use strtok to get words.
+*/
