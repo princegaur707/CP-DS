ac55c0b766ce5b66dd9ecf55f15f3a332f1381f1
princegaur707 <princegaur1998@gmail.com>
2023-02-17 23:55:24 +0530

    file changed

--- Asteroid Collision.cpp
+++ Asteroid Collision.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
-    stack<int>s;
-    vector<int> asteroidCollision(vector<int>& a) {
-        int n = a.size();
-        for (int i = 0; i < n; i++) {
-            int x = a[i];
-            if (!s.empty() and s.top() > 0 and x < 0) {
-                bool flag = 1;
-
-                while (!s.empty() and s.top() > 0 and x < 0) {
-                    if (abs(x) == s.top()) {
-                        flag = false;
-                        s.pop();
-                        break;
-                    } else if (abs(x) > abs(s.top())) {
-                        flag = true;
-                        s.pop();
-                        continue;
-                    } else {
-                        flag = false;
-                        break;
-                    }
-                } if (flag == true) {
-                    s.push(x);
-                }
-            } else {
-                //No collision
-                s.push(x);
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+      int n = asteroids.size();
+      stack<int>s;
+      for(int i = 0; i < n; i++) {
+        int x = asteroids[i];
+        if (!s.empty() and s.top() > 0 and x < 0) {//condn necessary for collision to occur latest rock should move left and earlier to the right(earlier one is in stack)
+          bool flag = true;
+          while(!s.empty() and s.top() > 0 and x < 0) {
+            if(abs(x) == s.top()) {
+              s.pop();
+              flag = false;//both rocks destroyed no need to insert it
+              break;
             }
+            else if (abs(x) > s.top()) {
+              s.pop();
+              flag = true;//we need to insert as x will destory latest rock
+              continue;
+            }
+            else {
+              flag = false;//flag required to update here too otherwise x will be inserted at last in case of TC 10 2 -5
+              break;
+            }
+          }
+          if(flag == true)
+            s.push(x);
         }
-
-        vector<int>ans(s.size());
-
-        int k = s.size() - 1;
-        while (!s.empty()) {
-            ans[k--] = s.top();
-            s.pop();
-        }
-        return ans;
+        else
+          s.push(x);//No Collision
+      }
+      vector<int>v(s.size());
+      int k = s.size() - 1;
+      while(!s.empty()) {//inserting the rocks into vector array in reverse order
+        v[k--] = s.top();
+        s.pop();
+      }
+      return v;
     }
 };
