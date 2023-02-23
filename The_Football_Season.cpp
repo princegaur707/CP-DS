7d1f431d02f1afc34e56786695d957188b14ee09
princegaur707 <princegaur1998@gmail.com>
2023-02-23 23:55:19 +0530

    file added

---
+++ The_Football_Season.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+int x, y, GCD;
+
+void EED(int a, int b) {
+	//BASE CASE
+	if(b == 0) {
+		x = 1, y = 0;
+		GCD = a;
+		return;
+	}
+	EED(b, a % b);
+	int current_x = y;
+	int current_y = x - ((a / b) * y);
+	x = current_x;
+	y = current_y;
+}
+int inverseModulo(int a, int m) {
+	// if(__gcd(a, m) != 1) {
+	// 	return 0;
+	// }
+	EED(a, m);
+	return (x + m) % m;
+}
+int32_t main() {
+	int n;
+	int p;
+	int w;
+	int d;
+	cin >> n >> p >> w >> d;
+	int x, y, z;
+	int g = __gcd(w, d);
+	if(p % g != 0) {//we know this from Diophantine equation
+		cout << "-1" << endl;
+		return 0;
+	}
+	if(n * w < p) {//if point are greater if even winning every match it is not possible
+		cout << "-1" << endl;
+		return 0;
+	}
+	p = p / g; //p = p' from now
+	w = w / g; //w = w' from now
+	d = d / g; //d = d' from now
+	//now for y we know y = (p'' / d''), p'' = p'/w', d'' = d'/w'
+	y = ((p % w) * inverseModulo(d, w)) % w;
+	x = (p - y * d) / w;
+	if(x + y > n) {
+		cout << "-1" << endl;
+		return 0;
+	}
+	if(x < 0) {
+		cout << "-1" << endl;
+		return 0;
+	}
+	z = n - (x + y);
+	cout << x << " " << y << " " << z << " " << endl;
+	
+}
--- practice.cpp
+++ practice.cpp
@@ -1,22 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
-int gcd(int a, int b) {
-	return b == 0 ? a : gcd(b, a % b);
-}
-int main() {
-	int n;
-	cin >> n;
-	string a;
-	cin >> a;
-	int r = 0;
-	if(n == 0)
-		cout << a << endl;
-	else {
-		for(int i = 0; i < a.size(); i++) {
-			int n1 = r * 10 + (a[i] - 48);
-			r = n1 % n;
+// const int N = 0;
+#define int long long
+
+
+
+
+int32_t main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int a, b, q;
+		cin >> a >> b >> q;
+
+		int combine[40005] = {0};
+		int pre[40005] = {0};
+
+
+		for (int i = 0; i < a * b; i++) {
+			if (((i % a) % b) != ((i % b) % a)) {
+				combine[i] = 1;
+			}
+			pre[i] = (i == 0) ? combine[i] : combine[i] + pre[i - 1];
 		}
-		cout << gcd(n, r) << endl;
+
+		int count1 = 0;
+		int count2 = 0;
+		int ans = 0;
+
+		while (q--) {
+			int l, r;
+			cin >> l >> r;
+			// ans[l, r] = pre[r] - pre[l - 1];
+			l--;
+			int x = r / (a * b);
+			int y = r % (a * b);
+
+			int x1 = l / (a * b);
+			int y1 = l % (a * b);
+
+			count1 = ((x * pre[a * b - 1]) + pre[y]);
+			count2 = ((x1 * pre[a * b - 1]) + pre[y1]);
+
+			ans = count1 - count2;
+			cout << ans << " ";
+		}
+		cout << endl;
 	}
-	return 0;
 }
