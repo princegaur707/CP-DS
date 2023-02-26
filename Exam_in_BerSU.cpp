dfff28a8cdb846b2914d0e461a1bbc289d4c829b
princegaur707 <princegaur1998@gmail.com>
2023-02-26 23:55:20 +0530

    file added

--- Exam_in_BerSU.cpp
+++ Exam_in_BerSU.cpp
@@ -11,14 +11,13 @@ int main() {
 	int total = 0, x = 0, y;
 	for(int i = 0; i < n; i++) {
 		x = total - M + arr[i];
-		cout << "total: " << total << endl;
 		count = 0;
 		if(x > 0) {
 			for(int j = 100; j > 0; j--) {
 				y = freq[j] * j;
 				if(x <= y) {
 					count += (x + j - 1) / j;
-					cout << count << endl;
+					// cout << count << endl;
 					break;
 				}
 				count += freq[j];
@@ -30,3 +29,8 @@ int main() {
 		cout << count << " ";
 	}
 }
+/*Approach: we will be using frequency array it will be the best choice because
+second best option priority queue may lead to TLE when the sum we want to find may be 
+present in queue at last so additional logn will come in TC. but we want to do it
+in O(n) so we can notice here in question max time can student can take for test
+is 100 with which we can understand that frequencey array is best suitable here*/
