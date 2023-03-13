b93f8859372658c537f4e7ff89485c63768f8b70
princegaur707 <princegaur1998@gmail.com>
2023-03-13 23:55:29 +0530

    file added

--- Min_Cost_Climbing_Stairs.cpp
+++ Min_Cost_Climbing_Stairs.cpp
@@ -1,8 +1,11 @@
 class Solution {
 	int stair(vector<int>&cost, int i, int n, int *dp) {
-		if (i >= n) {
-			return 0;
-		}
+		if (i > n)
+			return 1e9;
+   		 if(i == n)
+      		return 0;
+		// if (i >= n) //can be combined as if 'i' goes out of bound it means can reach final point and no need add anything
+		// 	return 0;
 		if (dp[i] != -1) return dp[i];
 		int op1, op2;
 		op1 = op2 = INT_MAX;
@@ -19,7 +22,7 @@ public:
 		for (int i = 0; i < cost.size() + 1; i++) dp[i] = -1;
 		int op1 = stair(cost, 0, n, dp);
 
-		for (int i = 0; i < cost.size() + 1; i++) dp[i] = -1;
+		//for (int i = 0; i < cost.size() + 1; i++) dp[i] = -1;NO need to do this as dp will remain same
 		int op2 = stair(cost, 1, n, dp);
 
 		return min(op1, op2);
