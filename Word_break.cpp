79283a9c56937b4056d215030d6b91a15d4bd3e6
princegaur707 <princegaur1998@gmail.com>
2023-03-28 23:57:23 +0530

    file added

---
+++ Word_break.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    unordered_map<string, bool>m;
+    int *dp;
+    bool Solve(string s, int pos) {
+        if(pos == s.length())
+          return 1;
+        if(dp[pos] != -1)
+          return dp[pos];
+        for(int end = pos + 1; end <= s.length(); end++) {
+          string x = s.substr(pos, end - pos);
+          if(m.count(x) and Solve(s, end))
+            return dp[pos] = 1;
+        }
+        return dp[pos] = 0;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+      for(int i = 0; i < wordDict.size(); i++) 
+        m.insert({wordDict[i], true});
+      dp = new int[s.length() + 1];
+      for(int i = 0; i <= s.length(); i++) dp[i] = -1;
+      return Solve(s, 0);
+    }
+};
